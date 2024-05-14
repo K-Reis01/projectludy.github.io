43df8f766343511ac5d58c9dd0115f02eba48d78
K-Reis01 <166565323+K-Reis01@users.noreply.github.com>
2024-05-14 01:00:30 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var imgAtual = "loob.jpeg";
-var imgPositiva = "r-sim.png";
-
-function trocar(){
-    document.getElementById("figura").src =imgAtual;
-    let aux = imgAtual;
-    imgAtual = imgPositiva;
-    imgPositiva = aux;
-}
